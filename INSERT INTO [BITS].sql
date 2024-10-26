929474f046ca5525e7d7f7d93bf9e3845d1068f2
Rafael Organo <rafael.organo@consolsys.com>
2024-10-26 14:38:24 +0800

    Initial commit for script directory

---
+++ INSERT INTO [BITS].sql
@@ -0,0 +1,28 @@
+TRUNCATE TABLE [BEYBLADE_MATCH_MAKER]..[BITS]
+
+INSERT INTO [BEYBLADE_MATCH_MAKER]..[BITS] VALUES
+	(NEWID(), 'Flat', 'F'),
+	(NEWID(), 'Taper', 'T'),
+	(NEWID(), 'Ball', 'B'),
+	(NEWID(), 'Needle', 'N'),
+	(NEWID(), 'High Needle', 'HN'),
+	(NEWID(), 'Low Flat', 'LF'),
+	(NEWID(), 'Point', 'P'),
+	(NEWID(), 'Orb', 'O'),
+	(NEWID(), 'Spike', 'S'),
+	(NEWID(), 'Rush', 'R'),
+	(NEWID(), 'High Taper', 'HT'),
+	(NEWID(), 'Gear Flat', 'GF'),
+	(NEWID(), 'Gear Ball', 'GB'),
+	(NEWID(), 'Gear Point', 'GP'),
+	(NEWID(), 'Gear Needle', 'GN'),
+	(NEWID(), 'Metal Needle', 'MN'),
+	(NEWID(), 'Elevate', 'E'),
+	(NEWID(), 'Hexa', 'H'),
+	(NEWID(), 'Quake', 'Q'),
+	(NEWID(), 'Accel', 'A'),
+	(NEWID(), 'Disc Ball', 'DB'),
+	(NEWID(), 'Unite', 'U'),
+	(NEWID(), 'Cyclone', 'C'),
+	(NEWID(), 'Dot', 'D'),
+	(NEWID(), 'Glide', 'G')
---
+++ INSERT INTO [BLADES].sql
@@ -0,0 +1,79 @@
+TRUNCATE TABLE [BEYBLADE_MATCH_MAKER]..[BLADES]
+
+INSERT INTO [BEYBLADE_MATCH_MAKER]..[BLADES] VALUES 
+	(NEWID(), 'Dranzer Spiral', 'Blue'),
+	(NEWID(), 'Cobalt Drake', 'Dark Blue'),
+	(NEWID(), 'Phoenix Feather', 'Red'),
+
+	(NEWID(), 'Dran Sword', 'Blue'),
+	(NEWID(), 'Dran Sword', 'Red'),
+	(NEWID(), 'Dran Sword', 'Black'),
+	
+	(NEWID(), 'Hells Scythe', 'Red'),
+	(NEWID(), 'Hells Scythe', 'Yellow'),
+	(NEWID(), 'Hells Scythe', 'Green'),
+	(NEWID(), 'Hells Scythe', 'Gold'),
+
+	(NEWID(), 'Wizard Arrow', 'Yellow'),
+	(NEWID(), 'Wizard Arrow', 'Red'),
+	(NEWID(), 'Wizard Arrow', 'Green'),
+	(NEWID(), 'Wizard Arrow', 'Blue'),
+	(NEWID(), 'Wizard Arrow', 'Amber'),
+	(NEWID(), 'Wizard Arrow', 'Purple'),
+
+	(NEWID(), 'Knight Shield', 'Green'),
+	(NEWID(), 'Knight Shield', 'Red'),
+	(NEWID(), 'Knight Shield', 'Blue'),
+	(NEWID(), 'Knight Shield', 'Sky Blue'),
+	(NEWID(), 'Knight Shield', 'Purple'),
+
+	(NEWID(), 'Knight Lance', 'Green'),
+	(NEWID(), 'Knight Lance', 'Yellow'),
+	(NEWID(), 'Knight Lance', 'Black'),
+
+	(NEWID(), 'Shark Edge', 'Prize'),
+	(NEWID(), 'Shark Edge', 'Yellow'),
+	(NEWID(), 'Shark Edge', 'Green'),
+	(NEWID(), 'Shark Edge', 'Black'),
+
+	(NEWID(), 'Leon Claw', 'Yellow'),
+	(NEWID(), 'Leon Claw', 'Red'),
+
+	(NEWID(), 'Viper Tail', 'Prize'),
+	(NEWID(), 'Viper Tail', 'Black'),
+	(NEWID(), 'Viper Tail', 'Yellow'),
+	(NEWID(), 'Viper Tail', 'Blue'),
+
+	(NEWID(), 'Rhino Horn', 'Purple'),
+
+	(NEWID(), 'Dran Dagger', 'Blue'),
+
+	(NEWID(), 'Hells Chain', 'Red'),
+
+	(NEWID(), 'Phoenix Wing', 'Red'),
+	(NEWID(), 'Phoenix Wing', 'Blue'),
+
+	(NEWID(), 'Wyvern Gale', 'Green'),
+	(NEWID(), 'Wyvern Gale', 'Yellow'),
+
+	(NEWID(), 'Unicorn Sting', 'Blue'),
+
+	(NEWID(), 'Sphynx Cowl', 'Prize'),
+	(NEWID(), 'Sphynx Cowl', 'Black'),
+	(NEWID(), 'Sphynx Cowl', 'White'),
+
+	(NEWID(), 'Leon Crest', 'Yellow'),
+
+	(NEWID(), 'Wizard Rod', 'Yellow'),
+
+	(NEWID(), 'Whale Wave', 'Prize'),
+	(NEWID(), 'Whale Wave', 'Black'),
+	(NEWID(), 'Whale Wave', 'White'),
+
+	(NEWID(), 'L-Drago', 'Upper'),
+	(NEWID(), 'L-Drago', 'Barrage'),
+
+	(NEWID(), 'Dran Buster', 'Blue'),
+
+	(NEWID(), 'Hells Hammer', 'Orange')
+
